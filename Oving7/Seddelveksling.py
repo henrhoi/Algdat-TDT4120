85343a4293f4dc903039861d19dc5b0dc0418da6
henrhoi <henrik.hoiness@online.no>
2017-10-15 11:45:45 +0200

    ..

--- Oving7/Seddelveksling.py
+++ Oving7/Seddelveksling.py
@@ -1,18 +1,18 @@
 from sys import stdin
 
-inf = 1000000000
-
 #Velger så mange av den største mynten, så nest størst osv
 def min_coins_greedy(coins, value):
     min_coins = 0
     for coin in coins:
-        min_coins += value//coin
-        value = value%coin
+        if value>=coin:
+            min_coins += value//coin
+            value = value%coin
 
     return min_coins
 
 
 def min_coins_dynamic(coins, value, length):
+    inf = 1000000000
     # Lager en antall minimum mynter
     # Starter på 0, så må legge til en på value
     min_coins = [inf]*(value+1)
@@ -21,7 +21,10 @@ def min_coins_dynamic(coins, value, length):
 
     for i in range(1,value+1):
         for j in range(length):
-            if coins[j] <= i:
+            if coins[j] == i:
+                min_coins[i] = 1
+
+            elif coins[j] < i:
                 # Før den aktuelle mynten blir lagt til
                 sub_resultat = min_coins[i-coins[j]]
                 # Om antall mynter nå er mindre enn det som var i tabellen fra før av
@@ -32,13 +35,10 @@ def min_coins_dynamic(coins, value, length):
 
 
 def can_use_greedy(coins):
-    for i in range(len(coins)-1):
-        if coins[i] % coins[i+1] != 0:
-
+    for i in range(1,len(coins)):
+        if coins[i]%coins[i-1] != 0:
             return False
-
     return True
-
 
 coins = sorted([int(c) for c in input().split()], reverse=True)
 method = stdin.readline().strip()
