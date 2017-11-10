13b88e948f908a137ba430878f375584993b8abd
henrhoi <henrik.hoiness@online.no>
2017-11-10 02:16:49 +0100

    test

--- push-algdat.py
+++ push-algdat.py
@@ -3,7 +3,7 @@ from sys import stdin
 
 stdin = input()
 
-call('cd Documents/GitHub/algdat', shell = True)
+#call('cd Documents/GitHub/algdat', shell = True)
 call('git add .', shell = True)
 call('git commit -m'+stdin, shell = True)
 call('git push', shell = True)
