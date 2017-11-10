7ff3974b733cc50d839bc429bb6e3d32257f62e1
henrhoi <henrik.hoiness@online.no>
2017-11-10 02:15:22 +0100

    helo

---
+++ push-algdat.py
@@ -0,0 +1,9 @@
+from subprocess import call
+from sys import stdin
+
+stdin = input()
+
+call('cd Documents/GitHub/algdat', shell = True)
+call('git add .', shell = True)
+call('git commit -m'+stdin, shell = True)
+call('git push', shell = True)
